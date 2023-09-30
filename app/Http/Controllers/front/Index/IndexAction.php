4db62b08087b4cb3eaa2f5d8afeb0c29918feac4
最初のコミットです。 <>
2023-10-01 02:49:50 +0900

    初期環境構築

---
+++ app/Http/Controllers/front/Index/IndexAction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\front\Index;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndexAction extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
