fb8dc7e5ae542bf391b6aeef34ce4e593bc15ce6
最初のコミットです。 <>
2023-10-07 21:45:50 +0900

    reset.cssをtailwindcssのものにした

--- resources/views/front/index/index.blade.php
+++ resources/views/front/index/index.blade.php
@@ -3,11 +3,6 @@
     <head>
         <meta charset="utf-8">
         <title>LuckyBeat</title>
-        <!-- reset.css destyle -->
-        <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"
-        />
         <link href="{{ asset('./css/app.css') }}" rel="stylesheet">
     </head>
     <body>
