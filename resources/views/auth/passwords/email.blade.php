bbb7e7ce14024b8e4a755a36ca6402a90a49c43a
最初のコミットです。 <>
2023-10-17 23:49:55 +0900

    パスワードリマインドのメールアドレス入力画面スタイル確定

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+<div class="white-form-holder">
+    <div class="form-header">
+        <a class="form-header-link" href="#">ログインページへ</a>
     </div>
-</div>
+
+    <div class="form-title">
+        <span class="form-title-text">パスワード再設定</span>
+    </div>
+
+    <div>
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+
+            <div class="input-holder">
+                <label for="email" class="input-label text-primary">メールアドレス</label>
+
+                <input id="email" type="email" class="input-form @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                {{-- @error('email') --}}
+                    <span class="input-error-message" role="alert">
+                        {{-- <strong>{{ $message }}</strong> --}}
+                        メールアドレス形式で入力してください
+                    </span>
+                {{-- @enderror --}}
+            </div>
+            <div class="button-holder">
+                <button type="submit" class="button button-success">
+                    {{-- TODO::右端に置く --}}
+                    トークンをメールに送る
+                </button>
+            </div>
+        </form>
+    </div>
 @endsection
