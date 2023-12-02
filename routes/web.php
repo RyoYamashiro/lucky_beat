f0d04e80a921f0142c0fa37812e1f319e3513da0
最初のコミットです。 <>
2023-12-02 20:59:19 +0900

    ルート修正

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\front\Beat\DetailAction;
-use App\Http\Controllers\front\Beat\CustomAction;
-use App\Http\Controllers\front\Beat\CustomEditAction;
-use App\Http\Controllers\front\Beat\EditAction as BeatEditAction;
-use App\Http\Controllers\front\contact\ShowContactFormAction;
-use App\Http\Controllers\front\Index\IndexAction;
-use App\Http\Controllers\front\User\DetailAction as UserDetailAction;
+use App\Http\Controllers\Front\Beat\DetailAction;
+use App\Http\Controllers\Front\Beat\CustomAction;
+use App\Http\Controllers\Front\Beat\CustomEditAction;
+use App\Http\Controllers\Front\Beat\EditAction as BeatEditAction;
+use App\Http\Controllers\Front\contact\ShowContactFormAction;
+use App\Http\Controllers\Front\Index\IndexAction;
+use App\Http\Controllers\Front\User\DetailAction as UserDetailAction;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\front\User\EditAction;
+use App\Http\Controllers\Front\User\EditAction;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,6 +30,6 @@ Route::get('/user/ryoyama/', UserDetailAction::class)->name('user.detail');
 
 Route::get('/beat/noon/detail', DetailAction::class)->name('beat.detail');
 Route::get('/beat/noon/custom', CustomAction::class)->name('beat.custom');
-Route::get('/beat/noon/edit', BeatEditAction::class)->name('beat.custom');
-Route::get('/beat/noon/edit/custom', CustomEditAction::class)->name('beat.custom');
+Route::get('/beat/noon/edit', BeatEditAction::class)->name('beat.edit');
+Route::get('/beat/noon/edit/custom', CustomEditAction::class)->name('beat.custom.edit');
 Route::get('/contact', ShowContactFormAction::class)->name('contact');
